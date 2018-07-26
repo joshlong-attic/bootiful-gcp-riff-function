83aa30647d9d9e2edffbc9805199297af7b2afca
joshlong <josh@joshlong.com>
2018-07-25 20:43:04 -0700

    polish the deployment scripts.

--- riff-push.sh
+++ riff-push.sh
@@ -1,8 +1,8 @@
 export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
-
-
 export GCP_PROJECT=$PROJECT_ID
 echo "GCP_PROJECT: $GCP_PROJECT"
 
-# --git-revision jar
-riff function create java bootiful-gcp-riff-function --git-repo https://github.com/joshlong/bootiful-gcp-riff-function.git --artifact "uppercase-0.0.1-SNAPSHOT.jar"  --handler "uppercase" --image gcr.io/$GCP_PROJECT/bootiful-gcp-riff-function
+FN_NAME=${1:-bgrf}
+echo "creating function ${FN_NAME}"
+
+riff function create java $FN_NAME --git-repo https://github.com/joshlong/bootiful-gcp-riff-function.git --artifact "uppercase-0.0.1-SNAPSHOT.jar"  --handler "uppercase" --image gcr.io/$GCP_PROJECT/$FN_NAME
--- riff-run.sh
+++ riff-run.sh
@@ -1,15 +1,16 @@
 
-
+FN_NAME=${1:-bgrf}
+echo "running ${FN_NAME}.."
 export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
 
 export GCP_PROJECT=$PROJECT_ID
 echo "GCP_PROJECT: $GCP_PROJECT"
 
 #export SERVICE_HOST=`kubectl get route upper -o jsonpath="{.status.domain}"`
-export SERVICE_HOST=bootiful-gcp-riff-function.default.example.com
+export SERVICE_HOST=${FN_NAME}.default.example.com
 echo "SERVICE_HOST: $SERVICE_HOST"
 
 export SERVICE_IP=`kubectl get svc knative-ingressgateway -n istio-system -o jsonpath="{.status.loadBalancer.ingress[*].ip}"`
 echo "SERVICE_IP: $SERVICE_IP"
 
-curl -w '\n' --header "Host:$SERVICE_HOST" --header "Content-Type: text/plain" http://${SERVICE_IP} -d knative
+curl -v -w '\n' --header "Host:$SERVICE_HOST" --header "Content-Type: text/plain" http://${SERVICE_IP} -d knative

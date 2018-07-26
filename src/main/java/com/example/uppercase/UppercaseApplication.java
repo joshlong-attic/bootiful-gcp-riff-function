3bf3cfb8742fb0e04966e454b7dc4858ecd624ca
joshlong <josh@joshlong.com>
2018-07-25 23:53:13 -0700

    up

--- src/main/java/com/example/uppercase/UppercaseApplication.java
+++ src/main/java/com/example/uppercase/UppercaseApplication.java
@@ -3,7 +3,6 @@ package com.example.uppercase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import reactor.core.publisher.Flux;
 
 import java.util.function.Function;
 
@@ -11,8 +10,8 @@ import java.util.function.Function;
 public class UppercaseApplication {
 
 		@Bean
-		Function<Flux<String>, Flux<String>> uppercase() {
-				return input -> input.map(String::toUpperCase);
+		Function<String, String> uppercase() {
+				return String::toUpperCase;
 		}
 
 		public static void main(String[] args) {

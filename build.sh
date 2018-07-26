a39c7c375baa41b9e94355702900102d0b26559f
joshlong <josh@joshlong.com>
2018-07-25 23:47:57 -0700

    up

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@ mvn -f `dirname $0`/pom.xml clean package
 cp `dirname $0`/target/uppercase-0.0.1-SNAPSHOT.jar `dirname $0`
 git commit -am up
 git push 
+export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+FN_NAME=${1:-uppercase}
+riff function create java $FN_NAME --git-repo https://github.com/joshlong/bootiful-gcp-riff-function.git --artifact "uppercase-0.0.1-SNAPSHOT.jar" --handler "uppercase" --image gcr.io/$PROJECT_ID/$FN_NAME
--- riff-push.sh
+++
@@ -1,3 +0,0 @@
-export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
-FN_NAME=${1:-uppercase}
-riff function create java $FN_NAME --git-repo https://github.com/joshlong/bootiful-gcp-riff-function.git --artifact "uppercase-0.0.1-SNAPSHOT.jar" --handler "uppercase" --image gcr.io/$PROJECT_ID/$FN_NAME

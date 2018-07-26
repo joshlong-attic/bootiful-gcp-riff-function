f3967d60d70b734d1b1b1511205d8913dc4e8d11
joshlong <josh@joshlong.com>
2018-07-25 23:42:19 -0700

    up

--- riff-run.sh
+++ riff-run.sh
@@ -1,3 +1,2 @@
-FN_NAME=${1:-bgrf}
-#riff service invoke $FN_NAME  --   --header "Content-type: text/plain" -d "hello knative" -o results.txt
-riff service invoke $FN_NAME  --   --header "Content-type: text/plain" -d "hello knative"  
+FN_NAME=${1:-uppercase}
+riff service invoke $FN_NAME  --   --header "Content-type: text/plain" -d "hello knative"
